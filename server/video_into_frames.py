0bcd1a719f3a2e9fb13f52d2d9acb07ff0b0c57b
yixin0829 <yixin0829@outlook.com>
2023-01-28 10:54:20 -0500

    BE video into frames change

--- server/video_into_frames.py
+++ server/video_into_frames.py
@@ -1,22 +1,44 @@
 import sys
 import cv2
 import pafy
+import os
+import shutil
 
 print(cv2.__version__)
 
 def extractImages(pathIn):
+    # Config temporary directory path for storing video frames
+    TEMP_DIR = "server/temp/"
+
     count = 1
     video = pafy.new(pathIn)
+
+    # Log metadata from video (pafy object)
+    print(f"Video title: {video.title}")
+    print(f"Video length in seconds: {video.length}")
+    print(f"Video category: {video.category}")
+    print(f"Video keywords: {video.keywords}")
+
     best = video.getbest(preftype="mp4")
     vidcap = cv2.VideoCapture(best.url)
-    success,image = vidcap.read()
+    success, image = vidcap.read()
+    print(f"Successfully read video metadata from the link. Start capturing frames.")
+
+    # Create temp/ directory for storage
+    try:
+        os.mkdir(TEMP_DIR)
+    except FileExistsError:
+        print(f"FileExistsError: Temporary directory already exists. Removing the directory beofre creating a new one.")
+        shutil.rmtree(TEMP_DIR)	
+        os.mkdir(TEMP_DIR)
+
     success = True
     while success:
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) 
         success,image = vidcap.read()
-        print ('Read a new frame: ', success)
-        cv2.imwrite("./temp/frame%d.jpg" % count, image)     # save frame as JPEG file
+        print (f'Read a new frame {count}: ', success)
+        cv2.imwrite(TEMP_DIR + "/frame%d.jpg" % count, image)     # save frame as JPEG file
         count = count + 1
 
 if __name__ == "__main__":
-    extractImages("https://www.youtube.com/?v=eXYYTMxB5RA")
+    extractImages("https://www.youtube.com/watch?v=n99xj9pd35g")
