c7d6a7c8683b7e68c11038e8244cbcaefad0ec0b
yixin0829 <yixin0829@outlook.com>
2023-01-28 21:06:28 -0500

    refactor the BE

--- server/video_into_frames.py
+++ server/video_into_frames.py
@@ -5,14 +5,9 @@ import shutil
 from dotenv import load_dotenv
 
 
-print(cv2.__version__)
-
-load_dotenv()  # take environment variables from .env.
-pafy.set_api_key(os.getenv("GOOGLE_API"))
-
-def extractImages(pathIn):
-    # Config temporary directory path for storing video frames
-    TEMP_DIR = "temp/"
+def extractImages(pathIn, TEMP_DIR="temp/"):
+    load_dotenv()  # take environment variables from .env.
+    pafy.set_api_key(os.getenv("GOOGLE_API"))
 
     count = 1
     video = pafy.new(pathIn)
@@ -32,20 +27,25 @@ def extractImages(pathIn):
     try:
         os.mkdir(TEMP_DIR)
     except FileExistsError:
-        print(f"FileExistsError: Temporary directory already exists. Removing the directory beofre creating a new one.")
-        shutil.rmtree(TEMP_DIR)	
+        print(
+            f"FileExistsError: Temporary directory already exists. Removing the directory beofre creating a new one."
+        )
+        shutil.rmtree(TEMP_DIR)
         os.mkdir(TEMP_DIR)
 
     success = True
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) 
-        success,image = vidcap.read()
-        print (f'Read a new frame {count}: ', success)
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
+        success, image = vidcap.read()
+        print(f"Read a new frame {count}: ", success)
         try:
-            cv2.imwrite(TEMP_DIR + "/frame%d.jpg" % count, image)     # save frame as JPEG file
+            cv2.imwrite(
+                TEMP_DIR + "/frame%d.jpg" % count, image
+            )  # save frame as JPEG file
         except Exception as e:
             break
         count = count + 1
+
 
 if __name__ == "__main__":
     extractImages("https://www.youtube.com/watch?v=Ef3zmMC0pxs")
